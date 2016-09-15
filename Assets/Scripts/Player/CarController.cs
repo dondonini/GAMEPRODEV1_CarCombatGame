dbecea0b19a18778f62eaac43bf2edab3119b20b
Edisson Flores <flores96@gmail.com>
2016-09-15 12:36:40 -0400

    GPD1 - Car Combat Game V 0.0.0.0
    - Full project uploaded

---
+++ Assets/Scripts/Player/CarController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo{
+
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool drive;
+	public bool steering;
+}
+
+public class CarController : MonoBehaviour
+{
+
+    public List<AxleInfo> axleList;
+    public float maxTorque = 1000f;
+    public float maxSteeringAngle = 45f;
+    public float antiRollValue = 5000f;
+    public Rigidbody rigidBody;
+
+    public void FixedUpdate()
+    {
+        float motor = maxTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleList)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.drive)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+
+            AntiRoll(axleInfo.leftWheel, axleInfo.rightWheel);
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+
+    public void AntiRoll(WheelCollider WheelL, WheelCollider WheelR)
+    {
+        WheelHit hit;
+        float travelL = 1.0f;
+        float travelR = 1.0f;
+
+        bool groundedL = WheelL.GetGroundHit(out hit);
+        if (groundedL)
+            travelL = (-WheelL.transform.InverseTransformPoint(hit.point).y - WheelL.radius) / WheelL.suspensionDistance;
+
+        bool groundedR = WheelR.GetGroundHit(out hit);
+        if (groundedR)
+            travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y - WheelR.radius) / WheelR.suspensionDistance;
+
+        float antiRollForce = (travelL - travelR) * antiRollValue;
+
+        if (groundedL)
+            rigidBody.AddForceAtPosition(WheelL.transform.up * -antiRollForce,
+                   WheelL.transform.position);
+        if (groundedR)
+            rigidBody.AddForceAtPosition(WheelR.transform.up * antiRollForce,
+                   WheelR.transform.position);
+    }
+
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+}
+
