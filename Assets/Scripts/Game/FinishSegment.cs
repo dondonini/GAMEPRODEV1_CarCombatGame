abd11f4347fa1882e32e198d741c192a6a2023ca
DONDONINI-PDV6\Edisson Flores <flores96@gmail.com>
2017-01-16 10:43:32 -0500

    GPD1 - Car Combat Game V 0.1.4.0
    I don't remember

---
+++ Assets/Scripts/Game/FinishSegment.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishSegment : MonoBehaviour {
+
+    public Transform m_endPosition;
+    public float animationDuration = 5.0f;
+    public EasingFunction.Ease animation1Type;
+    public EasingFunction.Ease animation2Type;
+
+    private bool touched = false;
+    private bool active = false;
+
+    private float currentTime = 0.0f;
+
+    private float animation1Duration, animation2Duration;
+
+	// Use this for initialization
+	void Start () {
+
+        animation1Duration = Mathf.Lerp(0.0f, animationDuration, 0.75f);
+        animation2Duration = Mathf.Lerp(0.0f, animationDuration, 0.25f);
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            touched = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (touched)
+        {
+            
+
+            currentTime += Time.deltaTime;
+        }
+	}
+}
